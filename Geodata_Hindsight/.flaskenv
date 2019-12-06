53c051b08abd24e987d85fd4b12a78a1fd0ae9e0
josh <josh@penguin>
2019-12-06 08:41:09 -0500

    Cleaning out the old project

--- Geodata_Hindsight/.flaskenv
+++
@@ -1,1 +0,0 @@
-FLASK_APP=Geodata_Hindsight.py
--- Geodata_Hindsight/app/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/index')
-@app.route('/homebase')
-def index():
-    user = {'username': 'Joshie Poshie'}
-    return render_template('index.html',title='Home' , user=user )
